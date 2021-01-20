25eb2c86ae5379ef7da3c7b741aecda8e55715ff
Prashant Joshi <scienceprashant27@gmail.com>
2021-01-20 13:06:59 +0530

    wiring up google api library , sending a user into the OAuth flow and rendering authentication status

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component {
+  state = { isSignedIn: null };
+  componentDidMount() {
+    window.gapi.load('client:auth2', () => {
+      window.gapi.client
+        .init({
+          clientId: process.env.REACT_APP_CLIENT_KEY,
+          scope: 'email',
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+        });
+    });
+  }
+  renderAuthButton() {
+    if (this.state.isSignedIn === null) {
+      return <div>I don't know if signed in</div>;
+    } else if (this.state.isSignedIn) {
+      return <div>You are signed in</div>;
+    } else {
+      return <div>You are not signed in</div>;
+    }
+  }
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+export default GoogleAuth;
