22ec56aaff594aae0b99cafc733b30d4dd890b36
Prashant Joshi <scienceprashant27@gmail.com>
2021-01-21 16:36:07 +0530

    recording user's id when they sign in using the gapi id

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -21,7 +21,7 @@ class GoogleAuth extends React.Component {
   //onAuthChange is called with a boolean
   onAuthChange = (isSignedIn) => {
     if (isSignedIn) {
-      this.props.signIn();
+      this.props.signIn(this.auth.currentUser.get().getId());
     } else {
       this.props.signOut();
     }
