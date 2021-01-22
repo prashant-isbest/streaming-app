3cac3efbd57013bf5ec5b74669f6d9e0390f0108
Prashant Joshi <scienceprashant27@gmail.com>
2021-01-22 12:37:39 +0530

    connecting redux form and creating forms

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+class StreamCreate extends React.Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div>
+        <form>
+          <Field name="title" />
+          <Field name="description" />
+        </form>
+      </div>
+    );
+  }
+}
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
-
-export default StreamCreate;
+export default reduxForm({
+  form: 'streamCreate',
+})(StreamCreate);
