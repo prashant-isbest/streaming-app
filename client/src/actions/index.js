d82dd7c52c8e921b41976539c7599adda2edf171
Prashant Joshi <scienceprashant27@gmail.com>
2021-01-23 15:50:20 +0530

    setting up api server and creating streams through action creator

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,6 @@
+import { formValues } from 'redux-form';
+import streams from '../apis/streams';
+
 export const signIn = (userId) => {
   return {
     type: 'SIGN_IN',
@@ -9,4 +12,8 @@ export const signOut = () => {
   return {
     type: 'SIGN_OUT',
   };
+};
+
+export const createStream = (formValues) => async (dispatch) => {
+  streams.post('/streams', formValues);
 };
