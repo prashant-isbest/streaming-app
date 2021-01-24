3f2f81dcf28161ed5134d7f839838b66e71f8afd
Prashant Joshi <scienceprashant27@gmail.com>
2021-01-24 17:11:51 +0530

    conditionally showing edit and deletebuttons

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -6,13 +6,28 @@ class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams();
   }
+
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
   renderList() {
     return this.props.streams.map((stream) => {
       return (
         <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
           <i className="large middle aligned icon camera" />
-          <div className="content">{stream.title}</div>
-          <div className="description">{stream.description}</div>
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
         </div>
       );
     });
@@ -28,7 +43,10 @@ class StreamList extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-  return { streams: Object.values(state.streams) };
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.currentUserId,
+  };
 };
 
 export default connect(mapStateToProps, { fetchStreams })(StreamList);
