a4ed53aa1cb92c8cb45fef8afed756bbe2850b83
Prashant Joshi <scienceprashant27@gmail.com>
2021-01-24 15:41:50 +0530

    using lodash omit function to delete property in state and mapKeys to convert array of objects to into object with keyvalue pair with key is id

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,7 +1,9 @@
 import streams from '../apis/streams';
-
+import _ from 'lodash';
 export default (state = {}, action) => {
   switch (action.type) {
+    case 'FETCH_STREAMS':
+      return { ...state, ..._.mapKeys(action.payload, 'id') };
     case 'FETCH_STREAM':
       return { ...state, [action.payload.id]: action.payload };
     case 'CREATE_STREAM':
@@ -11,7 +13,8 @@ export default (state = {}, action) => {
       //   newState[action.payload.id] = action.payload;
       //   return newState;
       return { ...state, [action.payload.id]: action.payload }; //square bracket is key interpolation
-
+    case 'DELETE_STREAM':
+      return _.omit(state, action.payload);
     default:
       return state;
   }
