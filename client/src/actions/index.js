85b6ac12869cf2f7bab310aabea0c5ba84a50e26
Prashant Joshi <scienceprashant27@gmail.com>
2021-01-27 14:06:15 +0530

    url based selection for editing a particular stream

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import { formValues } from 'redux-form';
 import streams from '../apis/streams';
+import history from '../history';
 
 export const signIn = (userId) => {
   return {
@@ -18,6 +19,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
   const { userId } = getState().auth;
   const response = await streams.post('/streams', { ...formValues, userId });
   dispatch({ type: 'CREATE_STREAM', payload: response.data });
+  history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) => {
